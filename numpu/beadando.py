b95239f8fc3dae057b1b21ed5eb70d213dba034d
HuszarDora <90604773+HuszarDora@users.noreply.github.com>
2022-12-07 15:39:32 +0100

    Add files via upload

---
+++ numpu/beadando.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+
+df_dba = pd.read_csv('DBA.csv')
+df_tlt = pd.read_csv('TLT.csv')
+df_vde = pd.read_csv('VDE.csv')
+df_xlv = pd.read_csv('XLV.csv')
+df_xme = pd.read_csv('XME.csv')
+
+print(1)
+pass
